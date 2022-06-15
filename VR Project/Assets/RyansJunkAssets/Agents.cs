3a91697a8eb5e21d15d6064a85aea802e11c6610
s191825 <90234313+s191825@users.noreply.github.com>
2022-06-15 11:54:24 +1000

    TryingNavMesh
    Experimentation with nav mesh.

---
+++ VR Project/Assets/RyansJunkAssets/Agents.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agents : MonoBehaviour
+{
+    [Tooltip("Variable description")]
+    public float spawnRadius;
+    public float spawnCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

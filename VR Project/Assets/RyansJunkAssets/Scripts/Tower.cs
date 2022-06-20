ea2a3a8855cd92d99a85149582158d8bd0e3c43f
Ryan Snelleksz <s191825@students.aie.edu.au>
2022-06-20 16:58:35 +1000

    Started Tower predictions and firing
    Is barely done and in broken state.

--- VR Project/Assets/RyansJunkAssets/Scripts/Tower.cs
+++ VR Project/Assets/RyansJunkAssets/Scripts/Tower.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Tower : MonoBehaviour
 {
+    public GameObject theManager; // Being set in inspector for now but should be done in code later by whatever is used to make the towers
+    SpawnManager spawnManager; 
+
+    public float projectileFlightTime;
+    public float range;
+
+    Vector3 predictedPosition;
+
+    bool readyToShoot = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnManager = theManager.GetComponent<SpawnManager>();
+        //rangeBoi.transform.position = new Vector3(transform.position.x + range, rangeBoi.transform.position.y, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (readyToShoot && spawnManager.enemyList.Count != 0)
+        {
+            float distance = float.MaxValue;
+            GameObject target = null;
+            foreach (GameObject enemy in spawnManager.enemyList)
+            {
+                if (Vector3.Distance(enemy.transform.position, gameObject.transform.position) <= range && Vector3.Distance(enemy.transform.position, gameObject.transform.position) < distance)
+                {
+                    distance = Vector3.Distance(enemy.transform.position, gameObject.transform.position);
+                    target = enemy;
+                }
+            }
+            if (target != null)
+            {
+                predictedPosition += target.GetComponent<NavMeshAgent>().velocity * projectileFlightTime;
+
+                Vector3 planarTarget = new Vector3(target.transform.position.x, 0, target.transform.position.z);
+                Vector3 planarPostion = new Vector3(transform.position.x, 0, transform.position.z);
+
+                float yOffset = 
+
+                readyToShoot = false;
+            }
+        }
     }
 }

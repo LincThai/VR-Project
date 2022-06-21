24d76b1c77ae3d73119aa1472383a682ede386a3
s191825 <90234313+s191825@users.noreply.github.com>
2022-06-21 12:30:23 +1000

    Added a bezier curve class
    PAIN. Added bezier curve class. Uses has 3 points that are vector 3.

--- VR Project/Assets/RyansJunkAssets/Scripts/Tower.cs
+++ VR Project/Assets/RyansJunkAssets/Scripts/Tower.cs
@@ -11,13 +11,28 @@ public class Tower : MonoBehaviour
     public float projectileFlightTime;
     public float range;
 
+    public GameObject thing;
+
     Vector3 predictedPosition;
+
+    BoxCollider boxCollider;
+    
+    BezierCurve shotCurve;
 
     bool readyToShoot = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider>();
+        shotCurve = new BezierCurve(new Vector3 (46.2f, 20.0f, -73.0f), new Vector3((46.2f + 20) / 2, 100, (-73.0f + 20) / 2), new Vector3(20, 0, 20));
+
+        for (float i = 0; i < 1.0f; i += 0.01f)
+        {
+            Vector3 location = new Vector3(shotCurve.FindX(i), shotCurve.FindY(i), shotCurve.FindZ(i));
+            Instantiate(thing, location, transform.rotation);
+        }
+
         spawnManager = theManager.GetComponent<SpawnManager>();
         //rangeBoi.transform.position = new Vector3(transform.position.x + range, rangeBoi.transform.position.y, transform.position.z);
     }
@@ -25,29 +40,29 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (readyToShoot && spawnManager.enemyList.Count != 0)
-        {
-            float distance = float.MaxValue;
-            GameObject target = null;
-            foreach (GameObject enemy in spawnManager.enemyList)
-            {
-                if (Vector3.Distance(enemy.transform.position, gameObject.transform.position) <= range && Vector3.Distance(enemy.transform.position, gameObject.transform.position) < distance)
-                {
-                    distance = Vector3.Distance(enemy.transform.position, gameObject.transform.position);
-                    target = enemy;
-                }
-            }
-            if (target != null)
-            {
-                predictedPosition += target.GetComponent<NavMeshAgent>().velocity * projectileFlightTime;
+        //if (readyToShoot && spawnManager.enemyList.Count != 0)
+        //{
+        //    float distance = float.MaxValue;
+        //    GameObject target = null;
+        //    foreach (GameObject enemy in spawnManager.enemyList)
+        //    {
+        //        if (Vector3.Distance(enemy.transform.position, gameObject.transform.position) <= range && Vector3.Distance(enemy.transform.position, gameObject.transform.position) < distance)
+        //        {
+        //            distance = Vector3.Distance(enemy.transform.position, gameObject.transform.position);
+        //            target = enemy;
+        //        }
+        //    }
+        //    if (target != null)
+        //    {
+        //        predictedPosition += target.GetComponent<NavMeshAgent>().velocity * projectileFlightTime;
 
-                Vector3 planarTarget = new Vector3(target.transform.position.x, 0, target.transform.position.z);
-                Vector3 planarPostion = new Vector3(transform.position.x, 0, transform.position.z);
+        //        Vector3 planarTarget = new Vector3(target.transform.position.x, 0, target.transform.position.z);
+        //        Vector3 planarPostion = new Vector3(transform.position.x, 0, transform.position.z);
 
-               // float yOffset = 
+        //       // float yOffset = 
 
-                readyToShoot = false;
-            }
-        }
+        //        readyToShoot = false;
+        //    }
+        //}
     }
 }
